a3edb7fa9b358d1d67583d371d529d09589a01ed
sangumee <super2451894@gmail.com>
2018-08-31 15:00:45 +0900

    Add Items
    Add Items

--- js/app.js
+++ js/app.js
@@ -89,11 +89,6 @@ function ChooseChar(value) {
 
 // Enemies our player must avoid
 var Enemy = function (x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.x = x;
     this.y = y;
     this.speed = speed;
@@ -108,17 +103,18 @@ Enemy.prototype.update = function (dt) {
     // all computers.
     this.x += this.speed * dt;
 
-    // when off canvas, reset position of enemy to move across again
+    // Reset the position again when you leave the canvas
     if (this.x > 550) {
         this.x = -100;
-        this.speed = 100 + Math.floor(Math.random() * 512);
+        this.speed = 50 + Math.floor(Math.random() * 512);
     }
 
-    // Check for collision between player and enemies
-    if (player.x < enemy.x + 60 &&
-        player.x + 37 > enemy.x &&
-        player.y < enemy.y + 25 &&
-        30 + player.y > enemy.y) {
+    // Check collision between player and enemies
+    if (player.x < this.x + 50 &&
+        player.x + 50 > this.x &&
+        player.y < this.y + 50 &&
+        50 + player.y > this.y) {
+        // Reset initial position
         player.x = 200;
         player.y = 380;
     }
@@ -129,6 +125,35 @@ Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+let Item = function (x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed=speed;
+    this.sprite = 'images/Heart.png';
+};
+
+
+Item.prototype.update = function (dt) {
+    this.x += this.speed * dt;
+    if (player.x < this.x + 50 &&
+        player.x + 50 > this.x &&
+        player.y < this.y + 50 &&
+        50 + player.y > this.y) {
+        // Reset initial position
+        score += 1;
+        player.x = 200;
+        player.y = 380;
+    }
+};
+// Item Render
+Item.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Check Item collision with player
+
+
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -136,9 +161,10 @@ let Player = function (x, y, speed) {
     this.x = x;
     this.y = y;
     this.speed = speed;
-    this.sprite = 'images/char-boy';
+    this.sprite = 'images/char-boy.png';
 };
 
+// Init Score setting
 let score = 0;
 
 Player.prototype.update = function () {
@@ -156,11 +182,13 @@ Player.prototype.update = function () {
     if (this.y < -50) {
         this.x = 200;
         this.y = 390;
+        // Increase score
         score += 1;
         console.log(score);
     }
 };
 
+// Handle Keypress
 Player.prototype.handleInput = function (keyPress) {
     switch (keyPress) {
         case 'left':
@@ -189,12 +217,17 @@ ChooseChar('char-boy');
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 let allEnemies = [];
-let player = new Player(200, 400, 70);
-var enemyPosition = [60, 140, 220];
-var enemy;
+let player = new Player(200, 400, 50);
+let enemyInitPosition = [60, 140, 220, 300];
+let enemy;
 
-enemyPosition.forEach(function (posY) {
-    enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
+// Item value Y setting
+let itemInitPosition = [60, 140, 220, 300];
+// Random locate
+let item =new Item(Math.random() * 512, itemInitPosition[Math.floor(Math.random()*itemInitPosition.length)]);
+
+enemyInitPosition.forEach(function (posY) {
+    enemy = new Enemy(0, posY, 50 + Math.floor(Math.random() * 512));
     allEnemies.push(enemy);
 });
 
